50e22cda5f432aed88ba0e7fba32fa70e4493809
EC2 Default User <ec2-user@ip-10-0-0-202.ap-southeast-2.compute.internal>
2022-03-30 10:35:36 +0000

    buildspec file updated

--- main.go
+++ main.go
@@ -15,7 +15,7 @@ func main() {
 
 		f := fib()
 
-		res := &response{Message: "Hello AABG Group"}
+		res := &response{Message: "Hello AABG Group test"}
 
 		for _, e := range os.Environ() {
 			pair := strings.Split(e, "=")
